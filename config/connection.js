093758520d3c22bf52f54b9fe3ca4412be4dac30
LouiseKMiller <louise.austex@gmail.com>
2016-07-30 08:02:02 -0500

    add new jawsDB info

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,33 @@
 // UT BOOT CAMP
 // LOUISE K MILLER
 //
-//Make the connection to the database and export
+//=============================================
+//  CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+//  WHICH CAN BE EITHER ON THE LOCAL COMPUTER OR DEPLOYED TO JAWSDB
+//
 //
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+
+var source = {
+
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: "burgers_db"
+    },
+
+    jawsDB: {
+        port: 3306,
+        host: 'jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'g1dy3n7smet4fyks',
+        password: "jz7sjorwhndcabs4",
+        database: "xmkr1xnnl31pcgvk"
+    }
+}
+var connection = mysql.createConnection(source.jawsDB);
+
 
 connection.connect(function(err) {
     if (err) {
